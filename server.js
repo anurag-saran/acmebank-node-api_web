6f9ddb3311b10021259b846b46d881971afc18de
gptestudent <gptestudent@gptestudent.com>
2017-04-20 14:04:58 -0400

    Enabled CORS

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 
+app.all("/api/*", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  return next();
+});
 
 
 app.get('/', function(req, res) {
