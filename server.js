ddd04fa76f8b08d3c95ebe58163503449fe1623d
gptestudent <gptestudent@gptestudent.com>
2017-04-23 16:57:53 -0400

    a

--- server.js
+++ server.js
@@ -5,18 +5,21 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 
-app.all("/api/*", function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-  return next();
-});
+//app.all("/api/*", function(req, res, next) {
+//  res.header("Access-Control-Allow-Origin", "*");
+//  res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+//  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+//  return next();
+//});
 
 
 app.get('/', function(req, res) {
 	res.send('Webpage API Root');
 });
 
+app.get('/test', function(req, res) {
+	res.send('Webpage API Root test');
+});
 
 app.use(express.static(__dirname + '/public'));
 
