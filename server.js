d5ad2e518d7347d10837e8b8bf62a0db3fe11a0f
gptestudent <gptestudent@gptestudent.com>
2017-04-24 18:27:59 -0400

    lowercase

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get('/customer/:id', function(req, res) {
     console.log("*** About To Connected:");
     var connected = infinispan.client({port: jdgPort, host: jdgHost}, {version: '2.2'});
     console.log("***  Connected:");
-
+    custID = custID.toLowerCase();
     var resFlag=0;
 //    var cust = {
 //        
@@ -97,7 +97,7 @@ app.post('/customer', function (req, res) {
 	var body = _.pick(req.body, 'firstName', 'lastName', 'faceBookId', 'cellPhone', 'email','age','faceBookIdInternal');
     var cellPhone = body.cellPhone;
     var firstandlastName = body.firstName + body.lastName;
-    randomcode = 
+     
     console.log("*** ssn:"+ Math.floor(100000000 + Math.random() * 900000000));
     console.log("*** tickets:"+  Math.floor(1 + Math.random() * 9));
     console.log("*** accidents:"+  Math.floor(1 + Math.random() * 9));
@@ -127,8 +127,9 @@ app.post('/customer', function (req, res) {
                     console.log("*****Record undefined");
                     client.put(cellPhone, JSON.stringify(body));
                     console.log("*****Record second insert");
-                    client.put(firstandlastName.lowercase(), JSON.stringify(body));
-                    res.json(util.format('Customer Not Found %s! but inserted into cache now', custID));
+                    client.put(firstandlastName.toLowerCase(), JSON.stringify(body));
+                    
+                    res.json(util.format('Customer Not Found %s! but inserted into cache now with keys:', cellPhone+":"+firstandlastName.toLowerCase()));
                     
                 } else {
                      console.log("*****Record exists");
