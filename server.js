3395ca43e37f54c9bc0159c9e33adc95a9807356
gptestudent <gptestudent@gptestudent.com>
2017-04-25 11:44:15 -0400

    updated library

--- server.js
+++ server.js
@@ -54,10 +54,12 @@ app.get('/customer/:id', function(req, res) {
 
 
 app.post('/customer', function (req, res) {
-	var body = _.pick(req.body, 'firstName', 'lastName', 'faceBookId', 'cellPhone', 'email','age','faceBookIdInternal');
+	var body = _.pick(req.body, 'firstName', 'lastName', 'faceBookId', 'cellPhone', 'email','age','faceBookIdInternal','facebookSenderID');
+    
     var cellPhone = body.cellPhone;
     var firstandlastName = body.firstName + body.lastName;
     var faceBookIdInternal =   body.faceBookIdInternal;
+    var facebookSenderID = body.facebookSenderIDl
     
     body.ssn = Math.floor(100000000 + Math.random() * 900000000);
     body.tickets = Math.floor(1 + Math.random() * 9);
@@ -65,7 +67,7 @@ app.post('/customer', function (req, res) {
     body.timeReport = Math.floor(Math.random() * 2);
     body.salaryCredited = Math.floor(Math.random() * 2);
     body.employeeNo = Math.floor(1000 + Math.random() * 9000);
-
+    
     var connected = infinispan.client({port: jdgPort, host: jdgHost}, {version: '2.2'});
 	connected.then(function (client) {
         client.get(cellPhone).then(
@@ -74,7 +76,8 @@ app.post('/customer', function (req, res) {
                     client.put(cellPhone, JSON.stringify(body));
                     client.put(firstandlastName.toLowerCase(), JSON.stringify(body));
                     client.put(faceBookIdInternal.toLowerCase(), JSON.stringify(body));
-                    res.json(util.format('Customer Not Found %s! but inserted into cache now with keys:', cellPhone+":"+firstandlastName.toLowerCase()));
+                    client.put(facebookSenderID.toLowerCase(), JSON.stringify(body));
+                    res.json(util.format('Customer Not Found %s! but inserted into cache now with keys:cellPhone', cellPhone+":firstandlastName:"+firstandlastName.toLowerCase()+":facebookSenderID:"+facebookSenderID+":faceBookIdInternal:"+faceBookIdInternal));
                     
                 } else {
                     res.json(util.format('Customer Exists', cellPhone));
